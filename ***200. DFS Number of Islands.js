141f18d40d064867641081205b29a6e32a9beff1
Monica Rao <monicaxrao@gmail.com>
2019-01-28 16:21:55 -0500

    Update ***200. DFS Number of Islands.js

--- ***200. DFS Number of Islands.js
+++ ***200. DFS Number of Islands.js
@@ -26,25 +26,25 @@ Output: 3
  * @return {number}
  */
 var numIslands = function(grid) {
-    
-    let row = grid.length
-    let column = grid[0].length
-    let count = 0
-    for(let i=0; i<row; i++){
-        for(let j=0; j<column; j++){
-            if(grid[i][j]=='1'){
-                count++
-                dfs(i,j)
-            }
-        }
+  if(grid.length===0) return 0
+  let rows=grid.length
+  let cols=grid[0].length
+  let count=0
+  function dfs(i,j){
+    if(i<0 || j<0 || i>=rows || j>=cols || grid[i][j]!=1) return
+    grid[i][j]='#'
+    dfs(i-1,j)
+    dfs(i+1,j)
+    dfs(i,j-1)
+    dfs(i,j+1)
+  }
+  for(let i=0;i<rows;i++){
+    for(let j=0;j<cols;j++){
+      if(grid[i][j]==1){
+        count++
+        dfs(i,j)
+      }
     }
-    function dfs(i,j){
-        if(i<0 || i>=row || j<0 || j>=column) return
-        grid[i][j]='#'
-        dfs(i+1, j)
-        dfs(i-1, j)
-        dfs(i, j+1)
-        dfs(i, j-1)
-    }
-    return count
+  }
+  return count    
 };
